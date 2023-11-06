78a35c928a78d3f441335e279fd673e133cc767d
julian-lm-cabezas <julian-harry.cabezas@leroymerlin.es>
2023-11-06 09:50:43 +0100

    join config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { configDefaults,mergeConfig, defineConfig as defineTestConfig } from 'vitest/config'
+import { version } from './package.json'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default  mergeConfig(
+  defineConfig({
+    plugins: [ vue() ],
+    server:{ port: 8080 },
+    define: { 'import.meta.env.VERSION': JSON.stringify(version)},
+    resolve: {
+      alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
     }
-  }
-})
+  }),
+  defineTestConfig({
+    test: {
+      environment: 'jsdom',
+      include: ['tests/**/*.test.ts'],
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
+      coverage:{
+        provider: 'v8',
+        enabled: true,
+        reporter: ['lcov'],
+        reportsDirectory: 'tests/coverage'
+        
+      }
+    }
+  })
+)
+  
